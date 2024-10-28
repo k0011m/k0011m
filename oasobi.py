6046e3bf0e1728bcdad8a3434d7840d8590f2a60
k0011m <111685730+k0011m@users.noreply.github.com>
2024-10-29 06:43:47 +0900

    Update oasobi.py

--- oasobi.py
+++ oasobi.py
@@ -1,22 +1,17 @@
 import socket
 
-class operate_server:
-    def port_creat():#ポート、サーバー設立
-        port_number = input("port番号を入力してください")
-        yorn = input(port_number+"で間違いないですか？(Y/N):")
-        while True:
-            if yorn == "y" or "Y" or "yes" or "Yes":
-                operate_server.port_connect(port_number)
-                #ここにメッセージ送信などの関数を入れる
-                break
-            else:
-                print("goodbye!")
-                break
 
-    def port_connect(port_number):
+class operate_server:
+
+
+    #port_creat()のportnumberを使ってサーバー設立
+    def port_connect(port):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(("", port))
+        server_socket.listen(5)
 
-        
-operate_server.port_creat()
-operate_server.port_connect
+
+
+port = int(input("port番号を入力してください"))
+operate_server.port_connect(port)
