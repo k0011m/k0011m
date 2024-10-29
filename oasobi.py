7a3d3070cb913dcf9ca2c2c2b6f645f0a136f88a
k0011m <111685730+k0011m@users.noreply.github.com>
2024-10-29 13:20:45 +0900

    Update oasobi.py

--- oasobi.py
+++ oasobi.py
@@ -1,9 +1,11 @@
 import socket
 
+yes_list = ["y","yes","Yes","Y"]
+no_list = ["n","no","No","N"]
+name = "unknown"
 
 class operate_server:
-
-
+    send_port_num = int
     #port_creat()のportnumberを使ってサーバー設立
     def port_connect(port):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,7 +13,30 @@ class operate_server:
         server_socket.bind(("", port))
         server_socket.listen(5)
 
+    def send_port_setting(send_port_setting):
+        send_port_setting = send_port_setting
 
+def start():
+    port_num = int(input("port番号を入力してください"))
+    operate_server.port_connect(port_num)
 
-port = int(input("port番号を入力してください"))
-operate_server.port_connect(port)
+def previw():
+    num = int(input("\n\nHello!\n1:view\n2:open_port_change\n3:send_message\n4:exit\n\n"))
+
+    if num == 1:#port_view
+        global name
+        name_y_or_n = input("あなたは" + name + "です。\n名前を変更しますか？[Y/N]")
+        if name_y_or_n in yes_list:
+            new_name = input("名前を設定してください:")
+            name = new_name
+
+        elif name_y_or_n in no_list:
+            print("OK,Goodbye!")
+    elif num == 2:#send_port_setting
+        send_port_input = int(input("送信先のportを入力してください"))
+        operate_server.send_port_setting(send_port_input)
+    #elif num == 5:
+
+while True:
+    previw()
+    
